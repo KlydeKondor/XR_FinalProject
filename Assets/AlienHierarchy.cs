5a7a8d905b1fe43f42493f18a6af7a9aadfe9a3f
KlydeKondor <kyle.kentner@gmail.com>
2022-12-05 13:59:17 -0600

    Adding in Erin's changes, plus Moon texture

---
+++ Assets/AlienHierarchy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienHierarchy : MonoBehaviour
+{
+    public GameObject alien, moonrock, moonrock1, moonrock2;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject == moonrock || other.gameObject == moonrock1 || other.gameObject == moonrock2) {
+            other.transform.parent = alien.transform;
+        }        
+        Debug.Log("error");
+    } 
+}
