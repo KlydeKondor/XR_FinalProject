35f0581106f19927c33f1e9219e94e726d42476c
KlydeKondor <kyle.kentner@gmail.com>
2022-12-05 09:32:41 -0600

    Added A* search algorithm

--- Assets/GenerateMap.cs
+++ Assets/GenerateMap.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class GenerateMap : MonoBehaviour
 {
     // PUBLIC DATA MEMBERS
+    public MoveRoverAStar roverCtrl;
     public GameObject waypoint;
     public GameObject edge;
     public Terrain ground;
     public Material lineMaterial;
 
     // PRIVATE DATA MEMBERS
+    // Debug flag
+    bool isDebug = true;
+    
     // Bounds for waypoints/paths
+    private const int NUM_WAYPOINTS_TEST = 9;
     private const int NUM_WAYPOINTS = 14; // Includes the start and end points
     private int numPaths = 3; // Number of paths from start to end (may overlap)
 
     // Bounds for coordinates
-    private float minX = -100f, maxX = 100f, minZ = 25f, maxZ = 225f, defaultY = -20f;
+    private float minX = -100f, maxX = 100f, minZ = 25f, maxZ = 80f, defaultY = -20f;
+    //private float minX = -100f, maxX = 100f, minZ = 25f, maxZ = 225f, defaultY = -20f;
 
     // Data structures for waypoints/edges
-    private GameObject[] waypoints = new GameObject[NUM_WAYPOINTS];
-    //private Dictionary<int, int> edges = new Dictionary<int, int>();
-    private List<int>[] edges = new List<int>[NUM_WAYPOINTS];
+    private GameObject[] waypoints;
+    private List<int>[] edges;
 
     // LineRenderer object for drawing edges
     private LineRenderer lineRenderer;
@@ -30,15 +35,31 @@ public class GenerateMap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Use NUM_WAYPOINTS for random, NUM_WAYPOINTS_TEST for debug
+        if (!isDebug)
+        {
+            waypoints = new GameObject[NUM_WAYPOINTS];
+            edges = new List<int>[NUM_WAYPOINTS];
+        }
+        else
+        {
+            waypoints = new GameObject[NUM_WAYPOINTS_TEST];
+            edges = new List<int>[NUM_WAYPOINTS_TEST];
+        }
+        
         // Initialize the lists in edges
-        for (int i = 0; i < NUM_WAYPOINTS; i++)
+        for (int i = 0; i < edges.Length; i++)
         {
             edges[i] = new List<int>();
         }
         
-        // Get randomized waypoints and edges
-        GetWaypoints();
-        GetEdges();
+        // Get randomized waypoints and edges (send true for debug mode)
+        GetMap(isDebug);
+
+        // Pass the map's references over to the A-Star controller
+        roverCtrl.Waypoints = waypoints;
+        roverCtrl.Edges = edges;
+        roverCtrl.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -47,7 +68,22 @@ public class GenerateMap : MonoBehaviour
         
     }
 
-    void GetWaypoints()
+    void GetMap(bool isDebug = false)
+    {
+        // Get a deterministic map if testing; otherwise, get a randomized map
+        if (isDebug)
+        {
+            GetWaypoints_Test();
+            GetEdges_Test();
+        }
+        else
+        {
+            GetWaypoints_Random();
+            GetEdges_Random();
+        }
+    }
+
+    void GetWaypoints_Random()
     {
         // Variables for instantiating waypoints and determining heights above-ground
         GameObject curWaypoint;
@@ -63,10 +99,10 @@ public class GenerateMap : MonoBehaviour
         curWaypoint = Instantiate(waypoint);
         heightBuff = ground.SampleHeight(new Vector3(0f, 0f, maxZ + 25f));
         curWaypoint.transform.position = new Vector3(0f, defaultY + heightBuff, maxZ + 25f);
-        waypoints[NUM_WAYPOINTS - 1] = curWaypoint;
+        waypoints[waypoints.Length - 1] = curWaypoint;
 
         // Randomly select coordinates for 12 waypoints, within a range of coordinates
-        for (int i = 1; i < NUM_WAYPOINTS - 1; i++)
+        for (int i = 1; i < waypoints.Length - 1; i++)
         {
             // Get random X and Y coordinates within the range
             float randX = Random.Range(minX, maxX);
@@ -84,7 +120,7 @@ public class GenerateMap : MonoBehaviour
         }
     }
 
-    void GetEdges()
+    void GetEdges_Random()
     {
         // Create numPaths routes from the start node to the end node, visiting waypoints semi-randomly
         for (int i = 0; i < numPaths; i++)
@@ -94,7 +130,7 @@ public class GenerateMap : MonoBehaviour
             int b = 0;
 
             // Loop until the end node is reached
-            while (b < NUM_WAYPOINTS - 1)
+            while (b < waypoints.Length - 1)
             {
                 // Set the current start index to the previous end index
                 a = b;
@@ -103,11 +139,12 @@ public class GenerateMap : MonoBehaviour
                 while (a == b)
                 {
                     // Minimum is a to prevent backtracking; if the random index is out of bounds, go to the end node
-                    b = Mathf.Min(Random.Range(a, a + NUM_WAYPOINTS / 2), NUM_WAYPOINTS - 1);
+                    b = Mathf.Min(Random.Range(a, a + waypoints.Length / 2), waypoints.Length - 1);
                 }
 
-                // Add b to the list of endpoints for index a
+                // Add b to the list of endpoints for index a, and vice versa (bidirectional)
                 edges[a].Add(b);
+                edges[b].Add(a);
                 Debug.Log("Adding " + a.ToString() + ", " + b.ToString());
 
                 // Get the GameObjects at a and b
@@ -135,7 +172,107 @@ public class GenerateMap : MonoBehaviour
         }
 
         // DEBUG: Print edge array
-        for (int i = 0; i < NUM_WAYPOINTS; i++)
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            Debug.Log("\n----Node " + i.ToString() + "----");
+            foreach (int endp in edges[i])
+            {
+                Debug.Log(endp);
+            }
+        }
+    }
+
+    void GetWaypoints_Test()
+    {
+        // Variables for instantiating waypoints and determining heights above-ground
+        GameObject curWaypoint;
+        float heightBuff;
+
+        // Set the start as a fixed point
+        curWaypoint = Instantiate(waypoint);
+        heightBuff = ground.SampleHeight(new Vector3(0f, 0f, 0f));
+        curWaypoint.transform.position = new Vector3(0f, defaultY + heightBuff, 0f);
+        waypoints[0] = curWaypoint;
+
+        // Set the end as a fixed point
+        curWaypoint = Instantiate(waypoint);
+        heightBuff = ground.SampleHeight(new Vector3(0f, 0f, maxZ + 25f));
+        curWaypoint.transform.position = new Vector3(0f, defaultY + heightBuff, maxZ);
+        waypoints[waypoints.Length - 1] = curWaypoint;
+
+        // Create array for intermediate nodes
+        Vector3[] vecArray = new Vector3[7];
+        vecArray[0] = new Vector3(-40f, 0f, 20f);
+        vecArray[1] = new Vector3(0f, 0f, 45f);
+        vecArray[2] = new Vector3(30f, 0f, 30f);
+        vecArray[3] = new Vector3(70f, 0f, 20f);
+        vecArray[4] = new Vector3(-20f, 0f, 60f);
+        vecArray[5] = new Vector3(20f, 0f, 60f);
+        vecArray[6] = new Vector3(40f, 0f, 60f);
+
+        // Randomly select coordinates for 7 waypoints, within a range of coordinates
+        for (int i = 1; i < waypoints.Length - 1; i++)
+        {
+            // Get the coordinates and the corresponding height
+            //heightBuff = ground.SampleHeight(new Vector3(vecArray[i].x, 0f, vecArray[i].z));
+            vecArray[i - 1].y = defaultY + ground.SampleHeight(new Vector3(vecArray[i - 1].x, 0f, vecArray[i - 1].z));
+
+            // Create a clone of the waypoint object and assign the random coordinates as its location
+            curWaypoint = Instantiate(waypoint);
+            curWaypoint.transform.position = vecArray[i - 1];
+
+            // Add curWaypoint to the array
+            waypoints[i] = curWaypoint;
+        }
+    }
+
+    void GetEdges_Test()
+    {
+        // Create the edges (represented unidirectionally here)
+        edges[0].Add(1);
+        edges[0].Add(2);
+        edges[0].Add(3);
+        edges[0].Add(4);
+        edges[1].Add(5);
+        edges[2].Add(5);
+        edges[3].Add(4);
+        edges[3].Add(7);
+        edges[4].Add(7);
+        edges[5].Add(6);
+        edges[6].Add(8);
+        edges[7].Add(8);
+
+        // Create edges
+        for (int i = 0; i < waypoints.Length - 1; i++)
+        {
+            foreach (int endp in edges[i])
+            {
+                // Get the GameObjects at a and b
+                GameObject startNode = waypoints[i];
+                GameObject endNode = waypoints[endp];
+
+                // Initialize lineRenderer (https://docs.unity3d.com/ScriptReference/LineRenderer.SetPosition.html)
+                GameObject newChild = new GameObject();
+                newChild.transform.parent = startNode.transform;
+                lineRenderer = newChild.AddComponent<LineRenderer>();
+                lineRenderer.material = lineMaterial;
+                lineRenderer.widthMultiplier = 0.2f;
+                lineRenderer.positionCount = lineVertices;
+
+                // Get a vector from startNode to endNode
+                Vector3 curLine = endNode.transform.position - startNode.transform.position;
+
+                // Render the line
+                for (int j = 0; j < lineVertices; j++)
+                {
+                    // Set the position for each vertex
+                    lineRenderer.SetPosition(j, startNode.transform.position + j * curLine);
+                }
+            }
+        }
+
+        // DEBUG: Print edge array
+        for (int i = 0; i < waypoints.Length; i++)
         {
             Debug.Log("\n----Node " + i.ToString() + "----");
             foreach (int endp in edges[i])
---
+++ Assets/MoveRoverAStar.cs
@@ -0,0 +1,265 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRoverAStar : MonoBehaviour
+{
+    // PRIVATE DATA MEMBERS
+    // Struct to facilitate the priority queue
+    private struct NodeCost
+    {
+        public NodeCost(int node, float cost)
+        {
+            Node = node;
+            Cost = cost;
+        }
+
+        public int Node { get; }
+        public float Cost { get; }
+    }
+    
+    // Data structures for waypoints/edges
+    private GameObject[] waypoints;
+    private List<int>[] edges;
+    private List<int> optimalPath = new List<int>();
+    
+    // Arrays for actual and heuristic costs and the path taken
+    private float[] gFunc;
+    private float[] hFunc;
+    private int[] origins;
+
+    // PUBLIC ACCESSORS
+    public GameObject[] Waypoints
+    {
+        get { return waypoints; }
+        set { waypoints = value; }
+    }
+
+    public List<int>[] Edges
+    {
+        get { return edges; }
+        set { edges = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, world!");
+        //TestEnqueue();
+
+        // Define gFunc (path cost) and initialize hFunc (array of heuristic costs)
+        gFunc = new float[waypoints.Length];
+        GetActualCosts();
+        hFunc = new float[waypoints.Length];
+        GetHeuristicCosts();
+
+        // Define origin (array of indices showing the path taken)
+        origins = new int[waypoints.Length];
+
+        // Call the A* algorithm
+        AStar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    bool AStar()
+    {
+        // Instantiate a priority queue for tracking the nodes to explore and a stack for tracking explored nodes
+        List<NodeCost> priorityQueue = new List<NodeCost>();
+
+        // Instantiate NodeCost struct curNode initialized to the first node and a cost of zero; add to queue
+        priorityQueue.Add(new NodeCost(0, 0f));
+        gFunc[0] = 0f;
+
+        // Define variables for checking the actual and heuristic costs of a node
+        float gCost;
+        float hCost;
+
+        // Remember the previous index
+        int prev = -1;
+        origins[0] = prev;
+
+        // Until there are no more nodes in the queue
+        while (priorityQueue.Count > 0)
+        {
+            // Assign to curNode and dequeue
+            NodeCost curNode = priorityQueue[0];
+            priorityQueue.Remove(curNode);
+
+            // Check if this is the destination node
+            if (curNode.Node == waypoints.Length - 1)
+            {
+                Debug.Log("------FINISHED------");
+                
+                // Create an output list and add the final node to it
+                int pathElem = curNode.Node;
+                optimalPath.Add(pathElem);
+
+                // Traverse backwards and append each predecessor node to the front of finalPath
+                while (origins[pathElem] != -1)
+                {
+                    pathElem = origins[pathElem];
+                    optimalPath.Insert(0, pathElem);
+                }
+
+                // DEBUG: Print the final path
+                foreach (int i in optimalPath)
+                {
+                    Debug.Log(i);
+                }
+
+                // Return true on success
+                return true;
+            }
+
+            // Expand curNode and determine the total cost for each neighbor; place in priorityQueue accordingly
+            foreach (int endp in edges[curNode.Node])
+            {
+                // Get the actual cost (i.e. g(n), which is along the path being explored)
+                gCost = gFunc[curNode.Node] + Vector3.Distance(waypoints[curNode.Node].transform.position,
+                    waypoints[endp].transform.position);
+
+                // Get the heuristic cost (i.e. h(n), which is the straight-line distance to the end)
+                hCost = hFunc[endp];
+
+                // If cost is less than the previous gFunc for endp, update
+                if (gCost < gFunc[endp])
+                {
+                    // Set curNode as this node's origin
+                    origins[endp] = curNode.Node;
+                    
+                    // Set gFunc[endp] to the updated path cost
+                    gFunc[endp] = gCost;
+
+                    // Temporarily remove the endp node from the queue
+                    UpdateQueue(endp, ref priorityQueue);
+
+                    // Create a new NodeCost struct and add to the priority queue
+                    NodeCost neighborNode = new NodeCost(endp, gCost + hCost);
+                    PriorityEnqueue(ref priorityQueue, ref neighborNode);
+                }
+
+                // Update prev
+                prev = curNode.Node;
+            }
+        }
+
+        // If the queue is consumed without reaching the end node, return null
+        return false;
+    }
+
+    void GetActualCosts()
+    {
+        // Intialize the actual cost for each node to a large number
+        for (int i = 0; i < gFunc.Length; i++)
+        {
+            gFunc[i] = 100000f;
+        }
+    }
+
+    void GetHeuristicCosts()
+    {
+        // Get the end node
+        GameObject endNode = waypoints[waypoints.Length - 1];
+        
+        // Use the straight-line distance from the current node to endNode as the heuristic cost
+        for (int i = 0; i < hFunc.Length; i++)
+        {
+            // Set the heuristic cost at node i
+            hFunc[i] = Vector3.Distance(waypoints[i].transform.position, endNode.transform.position);
+        }
+    }
+
+    void TestEnqueue()
+    {
+        List<NodeCost> nodes = new List<NodeCost>();
+        NodeCost nodeCost = new NodeCost(0, 10f);
+        PriorityEnqueue(ref nodes, ref nodeCost);
+        nodeCost = new NodeCost(0, 10f);
+        PriorityEnqueue(ref nodes, ref nodeCost);
+        nodeCost = new NodeCost(0, 20f);
+        PriorityEnqueue(ref nodes, ref nodeCost);
+        nodeCost = new NodeCost(0, 5f);
+        PriorityEnqueue(ref nodes, ref nodeCost);
+        nodeCost = new NodeCost(0, 7f);
+        PriorityEnqueue(ref nodes, ref nodeCost);
+    }
+
+    void PriorityEnqueue(ref List<NodeCost> q, ref NodeCost elem)
+    {
+        // Indexing variables
+        int start = 0, end = q.Count - 1, med = (start + end) / 2;
+
+        // Direction to insert (1 is right, -1 is left)
+        int dir = 1;
+
+        // Insert via binary search
+        while (start <= end)
+        {
+            // Get the current median
+            med = (start + end) / 2;
+
+            // Check if the element to be inserted is of lower or greater cost than the current median element
+            if (elem.Cost > q[med].Cost)
+            {
+                // Priority of elem is lower than the element at med; search the current right half
+                start = med + 1;
+                dir = 1;
+            }
+            else if (elem.Cost < q[med].Cost)
+            {
+                // Priority of elem is higher than the element at med; search the current left half
+                end = med - 1;
+                dir = -1;
+            }
+            else
+            {
+                // Priority is the same as the current median; insert to the right (give priority to existing element)
+                dir = 1;
+                break;
+            }
+        }
+
+        // Insert elem left or right of med
+        if (med + dir >= q.Count)
+        {
+            // Edge case; at end of list, so append normally
+            q.Add(elem);
+        }
+        else if (dir > 0)
+        {
+            // Insert at med + dir (just right of med; will shift rest of list right)
+            q.Insert(med + dir, elem);
+        }
+        else
+        {
+            // Insert at med (will shift med and rest of list right)
+            q.Insert(med, elem);
+        }
+        
+        // DEBUG: Print q
+        foreach (NodeCost c in q)
+        {
+            Debug.Log(c.Cost);
+        }
+    }
+
+    void UpdateQueue(int index, ref List<NodeCost> q)
+    {
+        // Remove the old element
+        for (int i = 0; i < q.Count; i++)
+        {
+            // Revisiting a node would be suboptimal by definition; Node values are unique
+            if (q[i].Node == index)
+            {
+                // Remove the element and exit
+                q.Remove(q[i]);
+                break;
+            }
+        }
+    }
+}
